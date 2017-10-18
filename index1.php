9fa97a5afd5710762e6311183f0e7555e0893c0d
Nilesh Modhwadia <luice.phillips@gmail.com>
2017-10-18 11:32:47 +0530

    added index file to new branch

--- index1.php
+++ index1.php
@@ -6,7 +6,7 @@
 <head> 
 <meta name="viewport" content="initial-scale=1.0, user-scalable=no"/> 
 <meta http-equiv="content-type" content="text/html; charset=UTF-8"/> 
-<title>Reverse Geocoding</title> 
+<title>Reverse Geocoding test</title> 
 
 <script type="text/javascript" src="http://maps.googleapis.com/maps/api/js?sensor=false"></script> 
 <script type="text/javascript"> 
